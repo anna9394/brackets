ac41e6406a2a547ebb4c050b51561b0eab1fb441
Anna <annaandreevna90394@gmail.com>
2021-02-12 14:04:01 +0300

    feat: add function imlementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let acc = 0;
+    let found = 1;
+    while(found === 1){
+      found = 0;
+      for( let i = 0; i < bracketsConfig.length; i++){
+        let arr = str.split(bracketsConfig[i][0]+bracketsConfig[i][1]);
+        if(arr.length > 1){
+          acc++
+          found = 1;
+          str = arr.join('');
+        }
+      }
+    }
+    let result = false;
+    if(str === ''){
+      result = true;
+    }
+    return result;
 }
